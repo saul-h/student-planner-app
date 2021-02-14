8cb2e1670cdf8b0dfca1e9f5dafadc247c04fc9c
Efrain Huizar <efrainfhuizar@gmail.com>
2021-02-14 11:36:12 -0800

    added degree tracker activity ui

---
+++ app/src/main/java/com/gseven/studentplanner/DegreeTrackerActivity.java
@@ -0,0 +1,15 @@
+package com.gseven.studentplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class DegreeTrackerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_degree_tracker);
+    }
+}
