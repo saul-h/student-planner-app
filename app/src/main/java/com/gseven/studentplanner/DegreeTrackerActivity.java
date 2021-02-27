a3b681de437427aaba0a4b8b3f6eefd01f5add6a
Efrain Huizar <efrainfhuizar@gmail.com>
2021-02-27 13:58:32 -0800

    Added AddNewCourseActivity UI and connected to DegreeTracker

--- app/src/main/java/com/gseven/studentplanner/DegreeTrackerActivity.java
+++ app/src/main/java/com/gseven/studentplanner/DegreeTrackerActivity.java
@@ -2,7 +2,9 @@ package com.gseven.studentplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class DegreeTrackerActivity extends AppCompatActivity {
@@ -11,5 +13,16 @@ public class DegreeTrackerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_degree_tracker);
+    }
+
+    /**
+     * Opens AddNewCourse Activity when user clicks AddNewCourse Button
+     * @param view
+     */
+    public void startAddNewCourse(View view){
+        Intent intent = new Intent(this, AddNewCourseActivity.class);
+
+        startActivity(intent);
+
     }
 }
