ccb025af06dd466a0ea48884b74f5c8926be7dd6
Efrain Huizar <efrainfhuizar@gmail.com>
2021-03-04 19:25:46 -0800

    Added click listeners for various buttons

--- app/src/main/java/com/gseven/studentplanner/EditCourseActivity.java
+++ app/src/main/java/com/gseven/studentplanner/EditCourseActivity.java
@@ -3,12 +3,47 @@ package com.gseven.studentplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+/**
+ * EditCourseActivity
+ * Takes a Course passed from the parent Activity and updates the Course based on user submitted form fields. Returns updated Course
+ */
 public class EditCourseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_course);
+    }
+
+    /**
+     * Retrieves a Course from parent Activity intent and updates the given Course based on user data entered in UpdateCourseActivity
+     * @param view
+     */
+    public void updateCourse(View view) {
+
+        Toast toast = Toast.makeText(this,"UpdateCourse Button pressed!", Toast.LENGTH_LONG);
+        toast.show();
+
+        //TODO: 1.) Update the given Course based on user submitted fields in UpdateCourseActivity form
+        //TODO: 2.) Update the Student list of Courses to replace the old Course with updated version of Course
+        //TODO: 2.) Notify user the course was successfully updated
+
+    }
+
+    /**
+     * Deletes the currently displayed Course in the form from the Student list of Courses
+     * @param view
+     */
+    public void deleteCourse(View view) {
+
+        Toast toast = Toast.makeText(this,"DeleteCourse Button pressed!", Toast.LENGTH_LONG);
+        toast.show();
+
+        //TODO: 1.) Remove Course from Student list of Courses
+        //TODO: 2.) Notify user removed course was successful
+
     }
 }
--- app/src/main/java/com/gseven/studentplanner/ViewAllCoursesActivity.java
+++ app/src/main/java/com/gseven/studentplanner/ViewAllCoursesActivity.java
@@ -2,7 +2,9 @@ package com.gseven.studentplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ViewAllCoursesActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ViewAllCoursesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_all_courses);
+    }
+
+    public void startEditCourse(View view) {
+        Intent intent = new Intent(this,EditCourseActivity.class);
+        startActivity(intent);
     }
 }
