30c9bc8f9af9dfe868ee0ae2e8ac449a0839658a
Efrain Huizar <efrainfhuizar@gmail.com>
2021-03-13 15:55:46 -0800

    Implemented DegreeTracker RecyclerView and Added Course class

--- app/src/main/java/com/gseven/studentplanner/DegreeTrackerActivity.java
+++ app/src/main/java/com/gseven/studentplanner/DegreeTrackerActivity.java
@@ -1,22 +1,77 @@
 package com.gseven.studentplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.gseven.studentplanner.data.model.Course;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Main activity for the Degree Tracker module.
+ * Displays overall degree progress completion.
+ * Displays current list of Courses which are not completed
+ */
 public class DegreeTrackerActivity extends AppCompatActivity {
+
+
+    private List<Course> courses;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_degree_tracker);
+
+        /** Hard coded Courses for testing/debugging. Will remove when we're able to persist with DB */
+
+        Course course1 = new Course("CECS 445",3,"Complete","Spring 2020",'A');
+        Course course2 = new Course("CECS 491B",3,"Complete","Spring 2020",'A');
+        Course course3 = new Course("CECS 378",3,"Complete","Spring 2020",'B');
+        Course course4 = new Course("CECS 453",3,"Complete","Spring 2020",'B');
+        Course course5 = new Course("CECS 476",3,"Complete","Spring 2020",'A');
+        Course course6 = new Course("ENGR 361",3,"Complete","Spring 2020",'A');
+        Course course7 = new Course("CECS 474",3,"Complete","Spring 2020",'B');
+        Course course8 = new Course("CECS 475",3,"Complete","Spring 2020",'B');
+        Course course9 = new Course("CECS 342",3,"Complete","Spring 2020",'B');
+
+        courses = new ArrayList<>();
+
+        courses.add(course1);
+        courses.add(course2);
+        courses.add(course3);
+        courses.add(course4);
+        courses.add(course5);
+        courses.add(course6);
+        courses.add(course7);
+        courses.add(course8);
+        courses.add(course9);
+
+
+        /** Create and initialize the RecyclerView and RecyclerView properties */
+        recyclerView = findViewById(R.id.recyclerView_degreeTracker);
+
+        RecyclerView.ItemDecoration divider = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+
+        recyclerView.addItemDecoration(divider);
+
+        DegreeTrackerRecyclerViewAdapter adapter = new DegreeTrackerRecyclerViewAdapter(this, courses);
+
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 
     /**
-     * Opens AddNewCourse Activity when user clicks AddNewCourse Button
+     * Opens AddNewCourse Activity when user clicks AddNewCourse Button. Retrieves new Course returned and adds to Course list
      * @param view
      */
     public void startAddNewCourse(View view){
@@ -25,7 +80,7 @@ public class DegreeTrackerActivity extends AppCompatActivity {
 
         startActivity(intent);
 
-        //TODO: Retrieve the returned Course from AddNewCourse intent and populate the student's list of Courses
+        //TODO: Retrieve the returned Course from AddNewCourse intent and update/populate the student's list of Courses
     }
 
     /**
@@ -33,6 +88,7 @@ public class DegreeTrackerActivity extends AppCompatActivity {
      * @param view
      */
     public void startAllCourses(View view) {
+
         Intent intent = new Intent(this, ViewAllCoursesActivity.class);
 
         //TODO: populate intent with list of Course to be displayed in the ViewAllCourses Activity
---
+++ app/src/main/java/com/gseven/studentplanner/DegreeTrackerRecyclerViewAdapter.java
@@ -0,0 +1,88 @@
+package com.gseven.studentplanner;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.gseven.studentplanner.data.model.Course;
+
+import java.util.List;
+
+public class DegreeTrackerRecyclerViewAdapter extends RecyclerView.Adapter<DegreeTrackerRecyclerViewAdapter.DegreeTrackerViewHolder> {
+
+    private List<Course> courses;
+
+    private Context context;
+
+    public DegreeTrackerRecyclerViewAdapter(Context context, List<Course> courses) {
+        this.courses = courses;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public DegreeTrackerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.degree_tracker_row_item,parent,false);
+
+        DegreeTrackerViewHolder holder = new DegreeTrackerViewHolder(view);
+
+        return holder;
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DegreeTrackerViewHolder holder, int position) {
+
+        holder.courseNameText.setText(this.courses.get(position).getCourseName());
+
+        holder.unitText.setText(String.valueOf(this.courses.get(position).getUnits()));
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast toast = Toast.makeText(context, "Selected: " + courses.get(position).getCourseName(), Toast.LENGTH_SHORT);
+
+                toast.show();
+
+
+
+
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return courses.size();
+    }
+
+
+    public class DegreeTrackerViewHolder extends RecyclerView.ViewHolder{
+        TextView courseNameText;
+        TextView unitText;
+        ConstraintLayout parentLayout;
+
+        public DegreeTrackerViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            courseNameText = itemView.findViewById(R.id.txt_courseNameitem);
+            unitText = itemView.findViewById(R.id.txt_courseUnitItem);
+
+            parentLayout = itemView.findViewById(R.id.layout_degreeItem);
+
+        }
+    }
+
+
+}
