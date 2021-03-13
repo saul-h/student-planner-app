a38ec50ac53024ab4d6d6a545172e33397af9a78
saul-h <saulhernandez217@gmail.com>
2021-03-12 19:30:38 -0800

    hooked up goal tracker main menu button

--- app/src/main/java/com/gseven/studentplanner/MainMenuActivity.java
+++ app/src/main/java/com/gseven/studentplanner/MainMenuActivity.java
@@ -15,12 +15,23 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        final ImageButton todo_list_button = findViewById(R.id.todo_list_button);
         final ImageButton degree_tracker_button = findViewById(R.id.degree_tracker_button);
+        final ImageButton planner_button = findViewById(R.id.planner_button);
+        final ImageButton goal_button = findViewById(R.id.goal_button);
 
         degree_tracker_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), DegreeTrackerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        goal_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GoalTrackerActivity.class);
                 startActivity(intent);
             }
         });
