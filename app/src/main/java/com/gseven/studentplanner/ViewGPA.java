dd17c41a0cc6a9410751ac52312f071dbea0a2db
Galen Nakama <gnakama17@punahou.edu>
2021-03-25 14:42:26 -1000

    minor update to activities for the signup, signup success, view gpa, and view planner realized that I had not made activities with the xml and only had the xml

---
+++ app/src/main/java/com/gseven/studentplanner/ViewGPA.java
@@ -0,0 +1,14 @@
+package com.gseven.studentplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewGPA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_g_p);
+    }
+}
