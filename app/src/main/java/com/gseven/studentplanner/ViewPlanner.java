7267463a864d786189635a219f74b748573957d7
Galen Nakama <gnakama17@punahou.edu>
2021-03-25 15:49:32 -1000

    added the buttons to go from ViewPlanner to MainMenuActivity and vice versa

--- app/src/main/java/com/gseven/studentplanner/ViewPlanner.java
+++ app/src/main/java/com/gseven/studentplanner/ViewPlanner.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
 public class ViewPlanner extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_planner);
+
+        button = (Button) findViewById(R.id.button5);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainMenu();
+            }
+        });
+    }
+
+    public void openMainMenu() {
+        Intent intent = new Intent(this, MainMenuActivity.class);
+        startActivity(intent);
     }
 }
