21a0be75c1262d9430661428a11bf9f5f1ed21f9
saulh <saulhernandez217@gmail.com>
2021-04-07 20:14:15 -0700

    Added insert, update, delete methods and listGoals query

--- app/src/main/java/com/gseven/studentplanner/data/entities/Goal.java
+++ app/src/main/java/com/gseven/studentplanner/data/entities/Goal.java
@@ -1,4 +1,26 @@
 package com.gseven.studentplanner.data.entities;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Goal {
+    @PrimaryKey(autoGenerate = true)
+    public int gid;
+
+    @ColumnInfo(name = "goal_name")
+    public String goalName;
+
+    @ColumnInfo(name = "user_name")
+    public String userName;
+
+    @ColumnInfo(name = "desc")
+    public String desc;
+
+    @ColumnInfo(name = "is_complete")
+    public boolean isComplete;
+
+    @ColumnInfo(name = "date_completed")
+    public String dateCompleted;
 }
