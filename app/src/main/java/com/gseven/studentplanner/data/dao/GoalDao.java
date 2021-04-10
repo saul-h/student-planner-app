12bea9a70f775f37747ae4a3861f4aaefc2bdb8b
saulh <saulhernandez217@gmail.com>
2021-04-10 16:05:19 -0700

    modified Goal and database. Still working on getting recycler to display goals

--- app/src/main/java/com/gseven/studentplanner/data/dao/GoalDao.java
+++ app/src/main/java/com/gseven/studentplanner/data/dao/GoalDao.java
@@ -1,4 +1,25 @@
 package com.gseven.studentplanner.data.dao;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.gseven.studentplanner.data.entities.Goal;
+
+@Dao
 public interface GoalDao {
+    @Insert
+    public void insertGoals(Goal... goals);
+
+    @Update
+    public void updateGoals(Goal... goals);
+
+    @Delete
+    public void deleteGoals(Goal... goals);
+
+    @Query("SELECT * FROM goal")
+    public Goal[] loadAllGoals();
+
 }
--- app/src/main/java/com/gseven/studentplanner/data/database/AppDatabase.java
+++ app/src/main/java/com/gseven/studentplanner/data/database/AppDatabase.java
@@ -1,4 +1,12 @@
 package com.gseven.studentplanner.data.database;
 
-public class AppDatabase {
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.gseven.studentplanner.data.dao.GoalDao;
+import com.gseven.studentplanner.data.entities.Goal;
+
+@Database(entities = {Goal.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract GoalDao goalDao();
 }
