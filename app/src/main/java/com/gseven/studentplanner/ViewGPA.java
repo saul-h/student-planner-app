99380b7a451bfbf0e9c2349a1f1f6f71101acade
Galen Nakama <gnakama17@punahou.edu>
2021-05-03 03:54:23 -1000

    updated viewGPA, hardcoding classes and still working on building a working GPA calculator as well as progress circle, button now works and sends user back to Degree Tracker

--- app/src/main/java/com/gseven/studentplanner/ViewGPA.java
+++ app/src/main/java/com/gseven/studentplanner/ViewGPA.java
@@ -2,13 +2,34 @@ package com.gseven.studentplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ViewGPA extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_g_p);
+
+        button = (Button) findViewById(R.id.button9);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDT();
+            }
+        });
+
     }
+
+    //Takes the user from the GPA to the Degree Tracker
+    public void openDT() {
+        Intent intent = new Intent(this, DegreeTrackerActivity.class);
+        startActivity(intent);
+    }
+
 }
